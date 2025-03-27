54dd707f5209ec095ef8d15fbf72e7b2bd2c2186
taoufik.mohamed <naciritaoufikmed@gmail.com>
2025-03-27 03:16:35 +0000

    fix: base path for deploiement

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -5,7 +5,8 @@ export const clientPaths = {
   postes: '/postes',
 } as const;
 
-const BASE_PATH = `http://localhost:3000/api`;
+const BASE_PATH =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
 
 export const apiPaths = {
   postes: () => `${BASE_PATH}/postes`,
